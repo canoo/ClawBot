f22680e716c4f8d4d6478200007a2f50d7a60dc4
Emiliano Martinez <cano.0708@hotmail.com>
2024-12-12 20:11:20 -0700

    added servo functionality and esp32 ino file

--- src/main.cpp
+++ src/main.cpp
@@ -12,36 +12,37 @@
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-
-//Pinouts using ATMega2560
-//Motor A
+// Pinouts using ATMega2560
+// Motor A
 int enA = 10;
 int in1 = 22;
 int in2 = 24;
 
-//Motor B
+// Motor B
 int enB = 11;
 int in3 = 26;
 int in4 = 28;
 int pos = 0;
 
-//Servo1 13
+// Servo objects and pin definitions
 Servo servo1;
-int servoPin1 = 13;
-//Servo2 12
 Servo servo2;
-int servoPin2 = 12;
-//acting as arms of a claw
-//servo1 will go from 180 - 90
-//servo2 will go from 0 - 90
+Servo servo3;
+Servo servo4;
+Servo servo5;
 
+int servoPin1 = 3;
+int servoPin2 = 4;
+int servoPin3 = 5;
+int servoPin4 = 6;
+int servoPin5 = 7;
 
 void setup() {
     Serial.begin(9600);
-    if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
-    Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
-  }
+    if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+        Serial.println(F("SSD1306 allocation failed"));
+        for (;;); // Don't proceed, loop forever
+    }
     display.display();
     delay(200);
     display.clearDisplay();
@@ -53,52 +54,42 @@ void setup() {
     display.clearDisplay();
     display.println("Hello, world!");
 
-
-pinMode(enA, OUTPUT);
-pinMode(in1, OUTPUT);
-pinMode(in2, OUTPUT);
-pinMode(enB, OUTPUT);
-pinMode(in3, OUTPUT);
-pinMode(in4, OUTPUT);
-
-servo1.attach(servoPin1);
-servo2.attach(servoPin2);
-servo1.write(0);
-servo2.write(360);
-
-
+    pinMode(enA, OUTPUT);
+    pinMode(in1, OUTPUT);
+    pinMode(in2, OUTPUT);
+    pinMode(enB, OUTPUT);
+    pinMode(in3, OUTPUT);
+    pinMode(in4, OUTPUT);
+        servo1.attach(servoPin1);
+        servo2.attach(servoPin2);
+        servo3.attach(servoPin3);
+        servo4.attach(servoPin4);
+        servo5.attach(servoPin5);
 }
 
-void loop() {
-    //write pos of servo1 and   servo2 to display while they spin 90deg every2 seconds
-    for (pos = 0; pos <= 360; pos += 10) {
-        servo1.write(pos);
-        servo2.write(360 - pos);
-        
-        display.clearDisplay();
-        display.setCursor(0,0);
-        display.print("Servo1: ");
-        display.println(servo1.read());
-        display.print("Servo2: ");
-        display.println(servo2.read());
-        display.display();
-        
-        delay(20);
-    }
-    delay(2000);
+String receivedMessage = "";
+int angle = 0;
 
-    for (pos = 360; pos >= 0; pos -= 10) {
-        servo1.write(pos);
-        servo2.write(360 - pos);
-        
+void loop() {
+    for (int angle = 0; angle <= 90; angle += 10) {
+        servo1.write(angle);
+        servo2.write(angle);
+        servo3.write(angle);
+        servo4.write(angle);
+        servo5.write(angle);
+        delay(100); // Adjust delay as needed
         display.clearDisplay();
-        display.setCursor(0,0);
+        display.setCursor(0, 0);
         display.print("Servo1: ");
-        display.println(servo1.read());
+        display.println(angle);
         display.print("Servo2: ");
-        display.println(servo2.read());
+        display.println(angle);
+        display.print("Servo3: ");
+        display.println(angle);
+        display.print("Servo4: ");
+        display.println(angle);
+        display.print("Servo5: ");
+        display.println(angle);
         display.display();
-        
-        delay(20);
     }
 }
